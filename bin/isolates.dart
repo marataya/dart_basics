041745c7391402cb7114b0d74155af5eea4bfb8a
marataya <marataya@gmail.com>
2023-06-28 14:31:40 +0600

    modify: playHideAndSeek, add fibonacci and computeFunction to isolates.dart

--- bin/isolates.dart
+++ bin/isolates.dart
@@ -3,26 +3,46 @@ import 'dart:isolate';
 import 'package:compute/compute.dart';
 
 void playHideAndSeek(SendPort sendPort) {
+  sendPort.send('Starting counting...');
   var counting = 0;
   for (var i = 1; i < 1000000000; i++) {
     counting = i;
   }
-  final message = '$counting, message from the Isolate';
-  Isolate.exit(sendPort, message);
+  sendPort.send('$counting, message from the Isolate');
+  sendPort.send(null);
 }
+
+Future<String> computeFunction(String msg) {
+  // Function doing long running work
+  return Future.delayed(const Duration(seconds: 3), () => msg);
+}
+
+int fibonacci(int n) {
+  if (n <= 0) return 0;
+  if (n == 1) return 1;
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
 
 Future<void> main() async {
   final receivePort = ReceivePort();
 
-  Isolate.spawn(playHideAndSeek, receivePort.sendPort);
-  final message = await receivePort.first;
-  print(message);
+  final isolate = await Isolate.spawn(playHideAndSeek, receivePort.sendPort);
 
-  Future<String> computeFunction() {
-    // Function doing long running work
-    return Future.delayed(const Duration(seconds: 3), () => 'Hello from computeFunction');
-  }
+  receivePort.listen((Object? message) {
+    if(message is String) {
+      print(message);
+    } else if (message == null) {
+      receivePort.close();
+      isolate.kill();
+    }
+  });
 
-  final message1 = await compute(computeFunction);
 
+
+  final message1 = await compute(computeFunction, 'Hello from computeFunction');
+  print(message1);
+
+  int resultOfFibo = await compute(fibonacci, 40);
+  print('Fibonacci result: $resultOfFibo');
 }
